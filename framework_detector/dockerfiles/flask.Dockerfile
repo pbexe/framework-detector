818fa6a25f023cc5fbc3195ecf70c0af35f88ca8
Miles Budden <git@miles.so>
2021-08-10 14:04:49 +0100

    The basic framework type can now be found

--- framework_detector/dockerfiles/flask.Dockerfile
+++ framework_detector/dockerfiles/flask.Dockerfile
@@ -1,6 +1,5 @@
 FROM alpine:3.8
 RUN apk add python3 py-pip && python3 -m ensurepip && pip install --upgrade pip && pip install flask
-# RUN easy_install pip
 COPY templates /app
 WORKDIR /app
 COPY . /app/
--- framework_detector/main.py
+++ framework_detector/main.py
@@ -1,6 +1,32 @@
 import importlib.resources
-from . import frameworks
+from framework_detector import frameworks
+from pathlib import Path
+import json
+from typing import Any
 
 
-def main() -> None:
-    print(importlib.resources.read_text(frameworks, 'test.json'))
+def detect(path: Path) -> str:
+    for value in importlib.resources.contents(frameworks):
+        if value.endswith(".json") and importlib.resources.is_resource(
+            frameworks, value
+        ):
+            data = json.loads(importlib.resources.read_text(frameworks, value))
+            if check_match(path, data):
+                return data["name"]
+
+
+def check_match(path: Path, data: dict[str, Any]) -> bool:
+    for file in data["detect"]:
+        if (path / file).exists():
+            if data["detect"][file] is None:
+                return True
+            else:
+                file_text = (path / file).read_text()
+                for substring in data["detect"][file]:
+                    if substring in file_text:
+                        return True
+    return False
+
+
+if __name__ == "__main__":
+    print(detect(Path.cwd()))
