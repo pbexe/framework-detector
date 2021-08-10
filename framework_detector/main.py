ede7239c9db768e4ab0d36fb74d3891229cfd61a
Miles Budden <git@miles.so>
2021-08-10 14:14:51 +0100

    Added docs

--- framework_detector/main.py
+++ framework_detector/main.py
@@ -1,11 +1,23 @@
 import importlib.resources
-from framework_detector import frameworks
-from pathlib import Path
 import json
+from pathlib import Path
 from typing import Any
+
+from framework_detector import frameworks
 
 
 def detect(path: Path) -> str:
+    """Detects the framework of a given path.
+
+    Args:
+        path (Path): The path to the file or directory to detect.
+
+    Raises:
+        KeyError: Raised when a framework is not found.
+
+    Returns:
+        str: The framework name.
+    """
     for value in importlib.resources.contents(frameworks):
         if value.endswith(".json") and importlib.resources.is_resource(
             frameworks, value
@@ -13,9 +25,19 @@ def detect(path: Path) -> str:
             data = json.loads(importlib.resources.read_text(frameworks, value))
             if check_match(path, data):
                 return data["name"]
+    raise KeyError("No known framework found")
 
 
 def check_match(path: Path, data: dict[str, Any]) -> bool:
+    """Checks if the path matches the given framework.
+
+    Args:
+        path (Path): The path to the file or directory to check.
+        data (dict[str, Any]): The data to check against.
+
+    Returns:
+        bool: Whether the framework matches the path.
+    """
     for file in data["detect"]:
         if (path / file).exists():
             if data["detect"][file] is None:
